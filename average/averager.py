bf0af5e9df2c7a9b782963f542a4f9713648f69f
mariiakzmn <mariiiakuzmina@gmail.com>
2022-12-01 10:49:42 +0000

    Debugged averager

--- average/averager.py
+++ average/averager.py
@@ -4,12 +4,23 @@ import numpy as np
 
 
 def run_averages(file_input='data_sample.csv', file_output='result_average.csv'):
+
     # Open the file to analyse
-    planes = np.loadtxt(file_input, dtype=int,  delimiter=',')
+    def skipper(fname, header=False):
+        with open(fname) as fin:
+            no_comments = (line for line in fin if not line.lstrip().startswith('#'))
+            if header:
+                next(no_comments, None) # skip header
+            for row in no_comments:
+                yield row
+    try:
+        planes = np.loadtxt(skipper(file_input), dtype=int,  delimiter=',')
+    except ValueError:
+        planes = np.loadtxt(skipper(file_input, header=True), dtype=int,  delimiter=',')
 
     # Calculates the averages through the sagittal/horizontal planes
     # and makes it as a row vector
-    averages = planes.mean(axis=1)[np.newaxis, :]
+    averages = planes.mean(axis=0)[np.newaxis, :]
 
     # write it out on my file
     np.savetxt(file_output, averages, fmt='%.1f', delimiter=',')
